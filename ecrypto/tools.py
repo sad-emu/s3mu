03da76f6de92da9254f3a37dcec86292e54ec335
sad-emu <sad-emu@goaway.cc>
2026-03-27 09:05:04 +1100

    Moving into tools

--- ecrypto/tools.py
+++ ecrypto/tools.py
@@ -4,3 +4,14 @@ def int_to_bytes(int_val, num_bytes):
 
 def bytes_to_int(bytes_val):
     return int.from_bytes(bytes_val, byteorder='big')
+
+import hashlib
+
+def get_md5(file_path: str, chunk_size: int = 4096):
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as f:
+        next_bytes = f.read(chunk_size)
+        while len(next_bytes) != 0:
+            md5_hash.update(next_bytes)
+            next_bytes = f.read(chunk_size)
+    return md5_hash.hexdigest()
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setuptools.setup(
      name="s3mu",
      version="0.0.1",
      python_requires=">=3.12",
-     packages=["ecrypto", "helpers", "ecrypto.ehash"],
+     packages=["ecrypto", "helpers"],
      install_requires=["boto3", "cryptography"],
 )
