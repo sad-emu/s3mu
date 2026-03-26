ce58403abadad832b901c4ca5c6bcfbaf8d46d52
sad-emu <sad-emu@goaway.cc>
2026-03-27 08:59:33 +1100

    Setting up sub packages

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setuptools.setup(
      name="s3mu",
      version="0.0.1",
      python_requires=">=3.12",
-     packages=["ecrypto", "helpers"],
+     packages=["ecrypto", "helpers", "ecrypto.hash"],
      install_requires=["boto3", "cryptography"],
 )
